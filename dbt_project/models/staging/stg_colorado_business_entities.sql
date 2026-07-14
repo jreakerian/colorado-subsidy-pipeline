5ca4e0ebc740ff42e6b1c4899ac27f87b846628d
kerian <kerianeminence7@gmail.com>
2026-07-14 01:42:32 -0400

    style(dbt/staging): rename columns to snake_case in stg_colorado_business_entities

--- dbt_project/models/staging/stg_colorado_business_entities.sql
+++ dbt_project/models/staging/stg_colorado_business_entities.sql
@@ -12,22 +12,22 @@ with source as (
 
 cleaned as (
   select
-    entityid,
-    entityname,
-    principaladdress1,
-    principaladdress2,
-    principalcity,
-    principalstate,
-    principalzipcode,
-    principalcountry,
-    entitystatus,
-    jurisdictonofformation,
-    entitytype,
-    cast(entityformdate as date) as entityformdate,
+    entityid                                        as entity_id,
+    entityname                                      as entity_name,
+    principaladdress1                               as principal_address_1,
+    principaladdress2                               as principal_address_2,
+    principalcity                                   as principal_city,
+    principalstate                                  as principal_state,
+    principalzipcode                                as principal_zip_code,
+    principalcountry                                as principal_country,
+    entitystatus                                    as entity_status,
+    jurisdictonofformation                          as jurisdiction_of_formation,
+    entitytype                                      as entity_type,
+    cast(entityformdate as date)                    as entity_form_date,
     case 
       when REGEXP_LIKE(principalzipcode, '^\\d{5}(-\\d{4})?$') then SUBSTRING(principalzipcode, 1, 5)
       else null
-    end as clean_zipcode
+    end                                             as clean_zip_code
   from source
   where principalstate = 'CO'
     and entitystatus = 'Good Standing'
