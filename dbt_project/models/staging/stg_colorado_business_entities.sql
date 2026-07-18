4fa22ef138f4cf3e56bc8ba5b3ff3ffc3e8f377b
kerian <kerianeminence7@gmail.com>
2026-07-18 00:12:54 -0400

    refactor(dbt/staging): update stg_colorado_business_entities.sql type casting and logic

--- dbt_project/models/staging/stg_colorado_business_entities.sql
+++ dbt_project/models/staging/stg_colorado_business_entities.sql
@@ -12,25 +12,20 @@ with source as (
 
 cleaned as (
   select
-    entityid                                        as entity_id,
-    entityname                                      as entity_name,
-    principaladdress1                               as principal_address_1,
-    principaladdress2                               as principal_address_2,
-    principalcity                                   as principal_city,
-    principalstate                                  as principal_state,
-    principalzipcode                                as principal_zip_code,
-    principalcountry                                as principal_country,
-    entitystatus                                    as entity_status,
-    jurisdictonofformation                          as jurisdiction_of_formation,
-    entitytype                                      as entity_type,
-    cast(entityformdate as date)                    as entity_form_date,
-    case 
-      when REGEXP_LIKE(principalzipcode, '^\\d{5}(-\\d{4})?$') then SUBSTRING(principalzipcode, 1, 5)
-      else null
-    end                                             as clean_zip_code
+    entityid                  as entity_id,
+    entityname                as entity_name,
+    principaladdress1         as principal_address_1,
+    principaladdress2         as principal_address_2,
+    principalcity             as principal_city,
+    principalstate            as principal_state,
+    principalzipcode          as principal_zip_code,
+    principalcountry          as principal_country,
+    entitystatus              as entity_status,
+    jurisdictonofformation    as jurisdiction_of_formation,
+    entitytype                as entity_type,
+    cast(entityformdate as date) as entity_form_date
   from source
-  where principalstate = 'CO'
-    and entitystatus = 'Good Standing'
+  where principalstate = 'CO'   -- structural: limit to Colorado records only
 )
 
 select * from cleaned
