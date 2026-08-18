94a87052988a28052a839ac2effc0fadff88d33f
kerian <kerianeminence7@gmail.com>
2026-08-18 18:00:52 -0400

    refactor(outputs.tf): update file

--- terraform/modules/aws_infra/outputs.tf
+++ terraform/modules/aws_infra/outputs.tf
@@ -8,25 +8,12 @@ output "snowflake_role_name" {
   value       = aws_iam_role.snowflake_role.name
 }
 
-output "api_integration_role_arn" {
-  description = "ARN of the shared IAM role used for Snowflake API integration (alias of snowflake_role_arn)"
-  value       = aws_iam_role.snowflake_role.arn
-}
 
 output "policy_arn" {
   description = "ARN of the IAM policy attached to the Snowflake integration role"
   value       = aws_iam_policy.lakehouse_rw_policy.arn
 }
 
-output "table_bucket_arn" {
-  description = "ARN of the S3 Tables bucket (Iceberg external volume for Silver and Gold)"
-  value       = aws_s3tables_table_bucket.data_lake.arn
-}
-
-output "table_bucket_name" {
-  description = "Name of the S3 Tables bucket"
-  value       = aws_s3tables_table_bucket.data_lake.name
-}
 
 output "general_purpose_bucket_name" {
   description = "Name of the general-purpose project S3 bucket (raw uploads, scripts, misc)"
