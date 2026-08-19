987f42a66ce8457d20c8487b6cefdc24effb5827
kerian <kerianeminence7@gmail.com>
2026-08-18 20:13:34 -0400

    chore(generate_schema_name.sql): remove file

--- include/dbt/dbt_project/macros/generate_schema_name.sql
+++
@@ -1,9 +0,0 @@
-{% macro generate_schema_name(custom_schema_name, node) -%}
-    {%- set env = target.name -%}  {# Evaluates to 'dev' or 'prod' based on CLI target #}
-
-    {%- if custom_schema_name is none -%}
-        {{ target.schema }}
-    {%- else -%}
-        {{ custom_schema_name | trim }}_{{ env }}
-    {%- endif -%}
-{%- endmacro %}
